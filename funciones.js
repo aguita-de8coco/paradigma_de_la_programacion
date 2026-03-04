3edddd6e63ca41a5834ccccfdfb9d3876879c8b2
aguita-de8coco <santiagorangelbonilla@gmail.com>
2026-03-04 16:03:58 -0500

    actualizar variable

--- funciones.js
+++ funciones.js
@@ -11,3 +11,27 @@ function Division(x){
 };
 
 console.log(Division(8))
+
+let resultado = 0;
+function SumarPrecios(a,b) {
+    return resultado = resultado +  a * b;
+    
+};
+
+
+console.log(SumarPrecios(20 , 9));
+console.log(SumarPrecios(7 , 5));
+console.log(SumarPrecios(2 , 4));
+
+let palabra = "";
+function palabram(a) {
+    return palabra = palabra +  a;
+    
+};
+
+
+console.log(palabram("hola"), ("mundo"));
+
+
+
+
