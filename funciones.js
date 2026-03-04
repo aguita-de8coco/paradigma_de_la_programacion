06b86a0783fe5e69ffe911b3c2cd41a6b4d459bc
aguita-de8coco <santiagorangelbonilla@gmail.com>
2026-03-04 15:33:15 -0500

    funciones

---
+++ funciones.js
@@ -0,0 +1,13 @@
+function Suma(x){
+    let accion = x + 5;
+    return accion
+};
+
+console.log(Suma(7));
+
+function Division(x){
+    let accion = x / 2
+    return accion
+};
+
+console.log(Division(8))
